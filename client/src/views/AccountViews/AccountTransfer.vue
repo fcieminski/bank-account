e6f8d9b8b164e668261eb3b961b7d9ec5bd9e57d
Filip Chamier-Ciemiński <f.cieminski@gmail.com>
2020-05-06 23:01:45 +0200

    making transfers works!

--- client/src/views/AccountViews/AccountTransfer.vue
+++ client/src/views/AccountViews/AccountTransfer.vue
@@ -1,83 +1,125 @@
 <template>
 	<section class="transfer">
-		<div>Wykonaj Przelew</div>
-		<div class="horizontal--divider"></div>
-		<div class="transfer__container">
-			<div class="container__account">
-				<div>
-					Wybierz konto
-				</div>
-				<div v-for="(account, key) in accounts" :key="key" @click="makeActiveAccount($event, account)" class="account--name">
-					{{ account.description }}
-				</div>
-			</div>
-			<div class="divider mr-5 ml-5"></div>
-			<div class="container__data">
-				<transition name="fade" mode="out-in">
-					<div :key="1" v-if="!activeAccount" class="d-flex align-center info--data">
-						<i class="material-icons mr-2">error</i>Najpierw wybierz konto!
+		<div v-if="!loading">
+			<div>Wykonaj Przelew</div>
+			<div class="horizontal--divider"></div>
+			<div class="transfer__container">
+				<div class="container__account">
+					<div>
+						Wybierz konto
 					</div>
-					<div :key="2" v-else>
-						<validation-observer v-slot="{ invalid }">
-							<form @submit.prevent="" action="">
-								<validation-provider
-									class="input__box"
-									rules="required"
-									name="Numer konta"
-									v-slot="{ errors }"
-								>
-									<label for="accountNumber">Numer konta</label>
-									<input class="input__main" name="accountNumber" type="number" />
-									<input-error left :error="errors[0]" />
-								</validation-provider>
-								<validation-provider
-									class="input__box"
-									rules="required"
-									name="Dane adresowe"
-									v-slot="{ errors }"
-								>
-									<label for="adress">Dane adresowe</label>
-									<textarea class="input__main" name="adress" type="text" />
-									<input-error left :error="errors[0]" />
-								</validation-provider>
-								<validation-provider
-									class="input__box"
-									rules="required"
-									name="Tytuł"
-									v-slot="{ errors }"
-								>
-									<label for="title">Tytuł przelewu</label>
-									<textarea class="input__main" name="title" type="text" />
-									<input-error left :error="errors[0]" />
-								</validation-provider>
-								<div class="input__container--right">
+					<div
+						v-for="(account, key) in accounts"
+						:key="key"
+						@click="makeActiveAccount($event, account)"
+						class="account--name"
+					>
+						{{ account.description }}
+					</div>
+				</div>
+				<div class="divider mr-5 ml-5"></div>
+				<div class="container__data">
+					<transition name="fade" mode="out-in">
+						<div :key="1" v-if="!activeAccount && !transferDone" class="d-flex align-center info--data">
+							<i class="material-icons mr-2">error</i>Najpierw wybierz konto!
+						</div>
+						<div :key="2" v-else-if="activeAccount && !transferDone">
+							<validation-observer v-slot="{ invalid }">
+								<form @submit.prevent="makeTransfer" action="">
 									<validation-provider
-										class="input__box input__box--small"
+										class="input__box"
 										rules="required"
-										name="Kwota"
+										name="Numer konta"
 										v-slot="{ errors }"
 									>
-										<label for="amount">Kwota</label>
-										<input class="input__main" name="amount" type="number" />
+										<label for="accountNumber">Numer konta</label>
+										<input
+											v-model="transferData.to.accountNumber"
+											class="input__main"
+											name="accountNumber"
+											type="number"
+										/>
 										<input-error left :error="errors[0]" />
 									</validation-provider>
-								</div>
-								<div class="actions--container">
-									<div :disabled="invalid" class="btn pa-2">
-										Wykonaj
+									<validation-provider
+										class="input__box"
+										rules="required"
+										name="Nazwa odbiorcy"
+										v-slot="{ errors }"
+									>
+										<label for="name">Nazwa odbiorcy</label>
+										<input
+											v-model="transferData.to.name"
+											class="input__main"
+											name="name"
+											type="text"
+										/>
+										<input-error left :error="errors[0]" />
+									</validation-provider>
+									<validation-provider
+										class="input__box"
+										rules="required"
+										name="Dane adresowe"
+										v-slot="{ errors }"
+									>
+										<label for="adress">Dane adresowe</label>
+										<textarea class="input__main" name="adress" type="text" />
+										<input-error left :error="errors[0]" />
+									</validation-provider>
+									<validation-provider
+										class="input__box"
+										rules="required"
+										name="Tytuł"
+										v-slot="{ errors }"
+									>
+										<label for="title">Tytuł przelewu</label>
+										<textarea
+											v-model="transferData.title"
+											class="input__main"
+											name="title"
+											type="text"
+										/>
+										<input-error left :error="errors[0]" />
+									</validation-provider>
+									<div class="input__container--right">
+										<validation-provider
+											class="input__box input__box--small"
+											rules="required"
+											name="Kwota"
+											v-slot="{ errors }"
+										>
+											<label for="amount">Kwota</label>
+											<input
+												v-model="transferData.amount"
+												class="input__main"
+												name="amount"
+												type="number"
+											/>
+											<input-error left :error="errors[0]" />
+										</validation-provider>
 									</div>
-								</div>
-							</form>
-						</validation-observer>
-					</div>
-				</transition>
+									<div class="actions--container">
+										<button :disabled="invalid" class="btn pa-2">
+											Wykonaj
+										</button>
+									</div>
+								</form>
+							</validation-observer>
+						</div>
+						<div v-else>
+							Przelew wykonany!
+						</div>
+					</transition>
+				</div>
 			</div>
 		</div>
+		<loading-indicator v-else />
 	</section>
 </template>
 
 <script>
 	import accountService from "@services/AccountService";
+	import historyService from "@services/HistoryService";
 	import InputError from "@utils/InputError";
 	import { extend } from "vee-validate";
 	import { required, email, alpha } from "vee-validate/dist/rules";
@@ -93,20 +135,33 @@
 		data() {
 			return {
 				activeAccount: null,
-				accounts: null
+				accounts: null,
+				loading: false,
+				transferData: {
+					date: new Date().toLocaleDateString(),
+					amount: null,
+					currency: "",
+					title: "",
+					from: null,
+					to: {
+						name: "",
+						accountNumber: ""
+					}
+				},
+				transferDone: false
 			};
 		},
 		components: {
 			InputError
 		},
 		created() {
-            this.loading = true;
+			this.loading = true;
 			accountService
 				.getUserAccounts(this.user._id)
 				.then(account => {
 					this.accounts = account;
 				})
-				.catch((e) => {
+				.catch(e => {
 					console.error("error!");
 				})
 				.finally(() => {
@@ -120,6 +175,18 @@
 			makeActiveAccount(event, account) {
 				this.activeAccount = account;
 				event.target.classList.toggle("active");
+			},
+			makeTransfer() {
+				const transfer = { ...this.transferData, from: this.user, currency: "PLN", name: "transfer" };
+				historyService
+					.create(this.user._id, transfer)
+					.then(data => {
+						this.transferDone = true;
+						this.activeAccount = null;
+					})
+					.catch(() => {
+						console.error("error!");
+					});
 			}
 		}
 	};
